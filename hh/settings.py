630526ad1ff97f840e574f06316adb2a26e07092
mkessler <michael.kessler@igd-r.fraunhofer.de>
2025-07-16 14:13:21 +0200

    FIX: fix media root

--- hh/settings.py
+++ hh/settings.py
@@ -15,7 +15,7 @@ import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 ROOT_DIR = (
     environ.Path(__file__) - 2
